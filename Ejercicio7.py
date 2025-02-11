e011f21c998fa8df1f1a1a31ef64af7dcff0f4d2
Saul <lopezbautistasaul12@gmail.com>
2025-02-11 07:40:12 -0600

    Primer ejercicio

---
+++ Ejercicio7.py
@@ -0,0 +1,10 @@
+# Solicitar datos al usuario
+a = float(input("Ingresa el primer número: "))
+b = float(input("Ingresa el segundo número: "))
+
+resta = a - b
+multiplicacion = a * b
+
+print(f"\nEl resultado de la resta de {a} - {b} es: {resta}\n")
+print(f"El resultado de la multiplicación de {a} * {b} es: {multiplicacion}\n")
+print("¡Programa realizado por Saul!")
